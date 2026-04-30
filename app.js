127689461f7207970effa87f439e83f7dba6e351
Erick Yuri <erickyurirodrigues@gmail.com>
2026-04-30 14:53:28 -0300

    Criacao de arquivos do projeto e estruturação inicial do html e css

---
+++ app.js
@@ -0,0 +1,15 @@
+// exemplo de estrutura de dados
+
+const meusTrajetos = [
+    {
+        id: 1714500000000,
+        nome: "Casa -> UFOP",
+        consumoPercentual: 15
+    },
+
+    {
+        id: 1714500005000,
+        nome: "UFOP -> Casa",
+        consumoPercentual: 10
+    }
+]
